2f6e4265c9f3c0b75ff88599572dd4e075b1bc4e
MatiasFernandez <jmfernandez225@comunidad.unnoba.edu.ar>
2025-05-20 18:30:58 -0300

    Create a Spring Initializr

---
+++ STC-Project/src/main/java/STC/example/STC/Project/StcProjectApplication.java
@@ -0,0 +1,13 @@
+package STC.example.STC.Project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StcProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StcProjectApplication.class, args);
+	}
+
+}
---
+++ STC-Project/src/test/java/STC/example/STC/Project/StcProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package STC.example.STC.Project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StcProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
